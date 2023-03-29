41bbf2260713614afd65a249fea95b7320f195d8
Aiden25451 <a.asprakis@gmail.com>
2023-03-29 14:00:13 -0400

    1.01

---
+++ BloodyMary/Mirror/static/Mirror/index.js
@@ -0,0 +1,32 @@
+var clicks = 0;
+
+function myfunction() {   
+
+    var text = document.getElementsByClassName("summon")[0]; 
+    clicks++;
+
+    if(clicks == 3) {
+        text.textContent = "BLOODYMARY";
+        text.style.fontSize = String(clicks * 3) + "vw" ;
+        var delayInMilliseconds = 1000; //1 second
+
+        setTimeout(function() {
+            text.textContent = "";
+            document.getElementsByClassName("text")[0].style.visibility = "visible";
+            document.getElementsByClassName("text")[0].style.fontSize = "2vw";
+            document.getElementsByClassName("text")[0].style.paddingBottom = "400px";
+            document.getElementById("ghost").style.width = "30vh";
+            document.getElementById("ghost").style.marginTop = "40vh";
+
+            setTimeout(function() {
+                var picture_row = document.getElementById("pic");
+                picture_row.parentNode.removeChild(picture_row);
+            }, delayInMilliseconds);
+            
+            
+        }, delayInMilliseconds);
+    } else if(clicks < 3){
+        text.textContent = "BLOODYMARY";
+        text.style.fontSize = String(clicks * 3) + "vw" ;
+    }
+}
