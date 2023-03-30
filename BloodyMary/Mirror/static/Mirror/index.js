d1aa5256c6e109f2e511ad735341fd236359cdd7
Aiden25451 <a.asprakis@gmail.com>
2023-03-30 11:27:40 -0400

    1.04

--- BloodyMary/Mirror/static/Mirror/index.js
+++ BloodyMary/Mirror/static/Mirror/index.js
@@ -6,7 +6,7 @@ function myfunction() {
     clicks++;
 
     if(clicks == 3) {
-        text.textContent = "BLOODYMARY";
+        text.textContent = "BLOODYMARY!!!";
         text.style.fontSize = String(clicks * 3) + "vw" ;
         var delayInMilliseconds = 1000; //1 second
 
@@ -14,13 +14,15 @@ function myfunction() {
             text.textContent = "";
             document.getElementsByClassName("text")[0].style.visibility = "visible";
             document.getElementsByClassName("text")[0].style.fontSize = "2vw";
-            document.getElementsByClassName("text")[0].style.paddingBottom = "400px";
-            document.getElementById("ghost").style.width = "30vh";
-            document.getElementById("ghost").style.marginTop = "40vh";
+            document.getElementsByClassName("text")[0].style.paddingBottom = "200px";
+            document.getElementById("ghost").style.width = "40vh";
+            document.getElementById("ghost").style.marginTop = "45vh";
 
             setTimeout(function() {
                 var picture_row = document.getElementById("pic");
                 picture_row.parentNode.removeChild(picture_row);
+                document.getElementsByClassName("button")[0].remove();
+                document.getElementsByClassName("mirror")[0].remove();
             }, delayInMilliseconds);
             
             
